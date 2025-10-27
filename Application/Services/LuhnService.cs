4fe4a3507e268897a401d57577ff00d3060eec5a
Dharshana Chandrasekara <65272642+dharshanac@users.noreply.github.com>
2025-10-27 17:27:48 +1300

    Add Luhn algorithm validation

--- Application/Services/LuhnService.cs
+++ Application/Services/LuhnService.cs
@@ -4,9 +4,34 @@ namespace ZeilCardValidatorApi.Application.Services
 {
     public class LuhnService : ILuhnService
     {
+        // Remove spaces and dashes
+        private static readonly Regex _cleanup = new("[\\s-]+", RegexOptions.Compiled);
+
         public bool Validate(string rawCardNumber)
         {
-            return true;
+            if (string.IsNullOrWhiteSpace(rawCardNumber))
+                return false;
+
+            int sum = 0;
+            bool alternate = false;
+            var digitsOnly = _cleanup.Replace(rawCardNumber, string.Empty);
+
+            for (int i = digitsOnly.Length - 1; i >= 0; i--)
+            {
+                if (!char.IsDigit(digitsOnly[i]))
+                    return false;
+
+                int n = digitsOnly[i] - '0';
+
+                if (alternate)
+                {
+                    n *= 2;
+                    if (n > 9) n -= 9;
+                }
+                sum += n;
+                alternate = !alternate;
+            }
+            return sum % 10 == 0;
         }
     }
 }
